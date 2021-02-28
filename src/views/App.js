c6810e0ac13f1030665d7b0f8065e6bf7069c8ce
Will Cohen <whysgai@gmail.com>
2021-02-28 13:03:17 -0500

    A3 chore: Move things around

---
+++ src/views/App.js
@@ -0,0 +1,12 @@
+import logo from '../media/logo.svg';
+import '../styles/App.css';
+
+const App = () => {
+    return (
+        <main className="container">
+
+        </main>
+    )
+}
+
+export default App;
