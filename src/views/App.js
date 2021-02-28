ad51d407b6e03185aca96f5f2ae6ee7cf76db279
Will Cohen <whysgai@gmail.com>
2021-02-28 13:55:25 -0500

    A3 feat: Implement standard READ request

--- src/views/App.js
+++ src/views/App.js
@@ -1,10 +1,27 @@
-import logo from '../media/logo.svg';
+import {useState, useEffect} from 'react';
 import '../styles/App.css';
 
+const URL = "https://opentdb.com/api.php";
+
+const readQuestions = async () => {
+    let response = await fetch(URL + "?amount=10");
+    let questions = await response.json();
+    return questions;
+}
+
 const App = () => {
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(() => {
+        readQuestions().then(data => {
+            console.log("data:", data);
+            setQuestions(data.results);
+        })
+    }, []);
+
     return (
         <main className="container">
-
+               {console.log(questions)}
         </main>
     )
 }
