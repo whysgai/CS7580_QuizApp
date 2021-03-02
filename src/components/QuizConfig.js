d84185ee209952056dbe37d39f150b23dd923387
Will Cohen <will.r.cohen@gmail.com>
2021-03-02 16:19:11 -0500

    build-config feat: add basic quiz config card

--- src/components/QuizConfig.js
+++ src/components/QuizConfig.js
@@ -1,3 +1,8 @@
-const QuizConfig = props => {
-    
-}
+const QuizConfig = props => 
+    <div className="card card-body">
+        <label>
+            Number of questions:
+        </label>
+    </div>
+
+export default QuizConfig
