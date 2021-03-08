e5d1a686006920be55853508ee2b680081d0f009
Will Cohen <will.r.cohen@gmail.com>
2021-03-08 18:07:51 -0500

    style-summary style: Add conditional coloring to displayed answers

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -3,12 +3,15 @@ const Summary = props =>
         <div class="card-header">
             <h2>Results</h2>
         </div>
-        <div className="card-body">
+        <div className="card-body summary">
             <h2 className="text-center">You got {props.score.points}/{props.score.correct_answers.length} questions correct</h2>
-            <ul className="results-list">
+            <ul className="results-list list-group">
             {
                 props.score.correct_answers.map((answer, index) => 
-                    <li key={index}>Question {index+1}: {answer ? "Right" : "Wrong"}</li>
+                    <li key={index} className={`list-group-item result ${answer ? "border-success text-success" : "text-danger border-danger"}`}>
+                        <h4>Question {index+1}:</h4>
+                        <h4>{answer ? "Right" : "Wrong"}</h4>
+                    </li>
                 )
             }
             </ul>
