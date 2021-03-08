1bdea462842b53ca4524814110b137c296c8601d
Will Cohen <will.r.cohen@gmail.com>
2021-03-08 17:18:37 -0500

    style-settings style: Add styling to settings inputs

--- src/components/QuizConfig.js
+++ src/components/QuizConfig.js
@@ -1,55 +1,71 @@
 import categories from '../data/categories'
 
 const QuizConfig = props => 
-    <div className="card card-body">
-        <label>
-            Number of questions:
-            <input type="number" min="1" max="50" value={props.queryParams.num}
-                onChange={e => props.setQueryParams(
-                    {
-                        ...props.queryParams,
-                        num: e.target.value
-                    }
-                )}
-            />
-        </label>
-        <label>
-            Difficulty:
-            <select value={props.queryParams.dif}
-                onChange={e => props.setQueryParams(
-                    {
-                        ...props.queryParams,
-                        dif: e.target.value
-                    }
-                )}
+    <div className="card">
+        <div class="card-header">
+            <h2>Configure Your Quiz</h2>
+        </div>
+        <div className="card-body config">
+            <div className="config-inputs">
+                <label className="form-label config-setting">
+                    Number of questions:
+                    <input className="form-control"
+                        type="number" min="1" max="50" 
+                        value={props.queryParams.num}
+                        onChange={e => props.setQueryParams(
+                            {
+                                ...props.queryParams,
+                                num: e.target.value
+                            }
+                        )}
+                    />
+                </label>
+                <label className="form-label config-setting">
+                    Difficulty:
+                    <select className="form-control"
+                        value={props.queryParams.dif}
+                        onChange={e => props.setQueryParams(
+                            {
+                                ...props.queryParams,
+                                dif: e.target.value
+                            }
+                        )}
+                    >
+                        <option value="any">Any Difficulty</option>
+                        <option value="easy">Easy</option>
+                        <option value="medium">Medium</option>
+                        <option value="hard">Hard</option>
+                    </select>
+                </label>
+                <label className="form-label config-setting">
+                    Category:
+                    <select className="form-control"
+                        value={props.queryParams.cat}
+                        onChange={e => props.setQueryParams(
+                            {
+                                ...props.queryParams,
+                                cat: e.target.value
+                            }
+                        )}
+                    >
+                        <option value="any">Any Category</option>
+                        {
+                            categories.categories.map((category, index) => 
+                                <option key={index} value={index + 9}>
+                                    {category}
+                                </option>
+                            )
+                        }
+                    </select>
+                </label>
+            </div>
+            <button className="btn btn-success config-confirm" 
+                onClick={() => props.setStatus(props.STATUS.LOADING)}
             >
-                <option value="any">Any Difficulty</option>
-                <option value="easy">Easy</option>
-                <option value="medium">Medium</option>
-                <option value="hard">Hard</option>
-            </select>
-        </label>
-        <label>
-            Category:
-            <select value={props.queryParams.cat}
-                onChange={e => props.setQueryParams(
-                    {
-                        ...props.queryParams,
-                        cat: e.target.value
-                    }
-                )}
-            >
-                <option value="any">Any Category</option>
-                {
-                    categories.categories.map((category, index) => 
-                        <option key={index} value={index + 9}>
-                            {category}
-                        </option>
-                    )
-                }
-            </select>
-        </label>
-        <button onClick={() => props.setStatus(props.STATUS.LOADING)}>GO</button>
+                <h2 className="big-button-text">GO</h2>
+                <span className="little-button-text">GO</span>
+            </button>
+        </div>
     </div>
 
 export default QuizConfig
